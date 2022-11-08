3caf3bf06f334d68621ba5d17e3fd5d371ce0012
hamda <hamda.baashe44@gmail.com>
2022-11-08 18:22:09 +0000

    I finished Js exercise-1

--- script.js
+++ script.js
@@ -1,1 +1,13 @@
-console.log("hello");
+ 
+ 
+ let x= 15;
+ let y= 30;
+ if (x > y){
+    console.log(x + " is greater than " + y);
+ }
+ else if  (y > x){
+    console.log(y + " is greater than " + x);
+ }
+ else {
+    console.log(x + " is equal to " + y);
+ }
